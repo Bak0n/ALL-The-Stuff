a4fc846ea63b3f977420afae70f0dc8bd86e7283
Bak0n <Bak0n@users.noreply.github.com>
2016-01-14 20:31:56 -0500

    First Commit!

--- Games/Snake-Eyes/CODE.js
+++ Games/Snake-Eyes/CODE.js
@@ -1,1 +1,18 @@
-alert('Coming Soon!')
+//Startup Code (starts up variables when needed, and runs a few prompts to define settings)
+var P1 = 0
+var Winner = 0
+var PlayerCount = prompt('How many players will be playing? \n Early versions will support 1 player only, 2, then 3 and 4 will be added next in that order\n\nThis feild will not do anything currently as only 1 is supported')
+var WinScore = prompt('How many points are required to win? \nDefaults to 100 if an unnaceptable value is given (under 1, over 1500, or not an actual number)')
+//Defaulting unacceptable values
+if (WinScore !== 1) {
+alert('Value unnacceptable, setting to 100')
+}
+
+if (To-Do-List) {
+//  TO-DO
+// - Fix syntax error 
+// - Finish unnaceptable value correction
+// - Finish turns
+// - Finish rolling
+// More to come ;-; 
+}
