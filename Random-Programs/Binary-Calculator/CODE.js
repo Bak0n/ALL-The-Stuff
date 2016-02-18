9d55e8a9cb769d3eaebd5fe7b1d229286c11fadf
Bak0n <Bak0n@users.noreply.github.com>
2016-02-18 18:18:02 -0500

    Added 12-Bit mode
    12-Bit mode allows for numbers above 255 to be calculated, the new maximum is 4096

--- Random-Programs/Binary-Calculator/CODE.js
+++ Random-Programs/Binary-Calculator/CODE.js
@@ -1,10 +1,13 @@
 var ToConvert = prompt("Enter a nunber from 0-255 and it'll be converted to binary")
 var DigitValue = 128
+var BitCount = 12
 var BinaryNumber = ['']
 if(ToConvert > 255) {
-alert("You've given a value over 255! \nThis will always output 11111111")
+  alert("You've given a value over 255! \n12-Bit mode will be enabled")
+  var DigitValue = 4096
+  var BitCount = 12
 }
-for (i = 0; i < 8; i++) {
+for (i = 0; i < BitCounyt; i++) {
   if (DigitValue > ToConvert) {
     BinaryNumber.push("0");
   } else {
