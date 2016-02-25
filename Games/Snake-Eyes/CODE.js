f66c8c8d7b5f71571208b15c5b6aba8d87e1ed68
Bak0n <Bak0n@users.noreply.github.com>
2016-02-25 14:12:39 -0500

    Begun script posting and added ULTRA SECURE key

--- Games/Snake-Eyes/CODE.js
+++ Games/Snake-Eyes/CODE.js
@@ -1,5 +1,34 @@
 /*
+ * NOTE: Functions are broken due to syntax error, EVERYTHING IS FAIL
+ */
+alert('LAUNCHING SNAKE EYES DEVELOPMENT BUILD 1');
+var Winner = 0;
+var PlayerTurn = 1;
+var Winner = 0
 
-Total rework in progress, nothing to see here
+function EndTurn() {
+    var PlayerTurn = PlayerTurn + 1
+    if PlayerTurn = 3 {
+        var PlayerTurn = 1
+    }
+}
 
-*/
+function Roll(x); {
+    //	Used MDN's number method here
+    if(x == 0xFF9348932904892058434587975934758437509392458230984029384093248304957290845784925742895748957894) {
+    var Dice1 = Math.random() * (6 - 0) + 0;
+    var Dice2 = Math.random() * (6 - 0) + 0;
+} }
+
+while (Winner == 0); {
+    alert('Player ' + PlayerTurn)
+    var ToRoll = confirm('Roll?')
+    if ToRoll = true {
+        Roll(0xFF9348932904892058434587975934758437509392458230984029384093248304957290845784925742895748957894);
+        alert('Your rolls are ' + Dice1 + ' and ' + Dice2)
+    } else {
+        EndTurn();
+    }
+}
+
+alert('Player ' + Winnner + ' wins!')
