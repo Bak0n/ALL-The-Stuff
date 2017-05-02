94c8f4cbc24464a66ca0dc502044ac80d159a1a7
Bak0n <Bak0n@users.noreply.github.com>
2017-05-02 18:07:00 -0400

    Create v1.js

---
+++ riceBot/devBuild/v1.js
@@ -0,0 +1,28 @@
+// Stackoverflow script to install jQuery through console
+
+javascript: (function(e, s) {
+    e.src = s;
+    e.onload = function() {
+        jQuery.noConflict();
+        console.log('jQuery injected');
+    };
+    document.head.appendChild(e);
+})(document.createElement('script'), '//code.jquery.com/jquery-latest.min.js')
+
+// Code to parse question and solve
+
+function gAnswer() {
+	var qCont = document.getElementById("question-title");
+	var qRaw = qCont.innerText;
+	var qArray = qRaw.split(" ");
+	var o1 = qArray[0];
+	var o2 = qArray[2];
+	var aFinal = o1 * o2
+	console.log("Raw question: " + qRaw + " Answer: " + aFinal);
+}
+
+// Parse answers and click on the correct one, determined from gAnswer();
+
+funcions pAnswers() {
+// Still working!
+}
